0404b9e86f1957ea202bf58cea5a3aa00ffc2fbd
R4mmst3in <pjmarpe@gmail.com>
2024-11-18 21:48:56 +0100

    Primera versión 02- tic-tac-toe

--- Midudev/projects/02-tic-tac-toe/src/App.jsx
+++ Midudev/projects/02-tic-tac-toe/src/App.jsx
@@ -1,35 +1,78 @@
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const TURNS = {
+  X: 'x',
+  O: 'o',
+}
 
+// const board=Array(9).fill(null)
+
+
+const Square=({children, isSelected, updateBoard, index}) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`;
+  
+  
+  const handleClick = () => {
+    updateBoard(index);
+  }
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
   )
 }
 
-export default App
+
+const App= () => {
+  
+  // const [ board, setBoard ] = useState(['x','o','o','o','x','x','x','o','x']);
+
+  
+  const [ board, setBoard ] = useState(Array(9).fill(null));
+
+  const [ turn, setTurn ] = useState(TURNS.X);
+
+  const [ winner, setWinner ] = useState(null);
+
+  const updateBoard = (index) => {
+    if (board[index]) return
+      const newBoard =[...board];
+      newBoard[index] = turn;
+      setBoard(newBoard);
+      
+      const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X; // 
+      setTurn(newTurn);
+  }
+
+  return (
+    <main className="board">
+
+      <h1>Tic Tac Toe</h1>
+
+      <section className="game">
+        {
+          board.map((_, index) => {
+            return (
+              <Square key={index} index={index} updateBoard={updateBoard}>
+                {board[index]}
+              </Square>
+            )
+          })
+        }
+      </section >
+      <section className="turn">
+        <Square isSelected={turn===TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn===TURNS.O}>
+          {TURNS.O}
+        </Square>
+      </section>
+        
+    </main>
+    )
+}
+
+export default App;
